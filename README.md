3867daf06aea34871634e84c48e86f4cc3b0910b
Mansi Joshi <mansijoshi1777@gmail.com>
2020-08-04 21:27:41 +0530

    Add comments

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 https://remix.ethereum.org/#optimize=false&evmVersion=null&version=soljson-v0.4.25+commit.59dbf8f1.js
 
+
+## Most Important Notes : 
+
+If you get any error in functions in your IDE change compiler and check.
+
+compiler version : 0.4.25+commit.59dbf8f1
+
+
+Most Important Note: When you integrate smart contract with html that time don't forgot to change ENVIRONMENT Javascript VM To Web3 provider.
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component, useEffect, useState } from 'react';
-// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
 
-// http://127.0.0.1:
-
+// http://127.0.0.1:8545
 
 
 
@@ -13,36 +11,36 @@ function App() {
   const [state, setstate] = useState({
     name: '',
     age: 0
-  })
+  })//For input values.
 
   const [getname, setgetname] = useState("");
   const [getage, setgetage] = useState(0);
 
-  const [account, setaccount] = useState(null);
 
-  const [Loading, setLoading] = useState(false);
 
-  // const web3 = new Web3(Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  const [account, setaccount] = useState(null); //account address
+
+  const [Loading, setLoading] = useState(false); //Loader..
+
+
 
   var web3 = new Web3();
-  web3.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  web3.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));//Set http provider
 
 
   web3.eth.getAccounts().then((result) => {
     setaccount(result[0]);
-  })
+  })// This getAccounts will get all the accounts but we need first one.
 
+
+  // To get your particular account's smart contract you need to pass ABI and Address of your account.
+
+  // ABI = The Application Binary Interface (ABI) is a data encoding scheme used in Ethereum for working with smart contracts
+  // Address = Contract address.
+
+  //This two things you need to copy from IDE and change here everytime when you change something in smart contract
 
   var coursetroContract = new web3.eth.Contract([
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "Coursetro",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
     {
       "constant": false,
       "inputs": [
@@ -56,6 +54,33 @@ function App() {
         }
       ],
       "name": "setInstructor",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getInstructor",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "Coursetro",
       "outputs": [],
       "payable": false,
       "stateMutability": "nonpayable",
@@ -77,32 +102,16 @@ function App() {
       ],
       "name": "Instructor",
       "type": "event"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getInstructor",
-      "outputs": [
-        {
-          "name": "",
-          "type": "string"
-        },
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
     }
-  ], "0x57f85143ed7CA5026D22c7fBf5B45F0FF20DdbFe");
+  ], "0x45f2BC0fccF0CA53D64dC3A37ba5612586033061");  //This both value you will get from your IDE.
+
+
 
 
 
   onchange = (event) => {
     setstate(({ ...state, [event.target.name]: event.target.value }));
-  }
+  }//Input values.
 
 
   coursetroContract.getPastEvents('Instructor', {
@@ -116,7 +125,7 @@ function App() {
       var getage = events[events.length - 1].returnValues['age']
       setgetname(getname);
       setgetage(getage);
-    });
+    }); //This will get all the events which get called. I get the last one (updated one) and then get returnvalues of that event.
 
 
 
@@ -125,7 +134,7 @@ function App() {
     // coursetroContract.methods.getInstructor().call().then((result) => {
     //   setgetname(result[0]);
     //   setgetage(result[1]);
-    // })
+    // })   //This is get method need to use call().
   })
 
 
@@ -135,11 +144,11 @@ function App() {
       setLoading(true);
       if (err) {
         setLoading(false);
-        console.log('oh no');
-      }
+        console.log('Not allowed except owner');
+      } //This is for error when owner and msg sender are not same.
     });
   }
-
+  //This is set method when you are set something you need to use send() with address of account.
 
 
 
