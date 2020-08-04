6712fec4feab859db03b49ea6d5422fd0048312f
Mansi Joshi <mansijoshi1777@gmail.com>
2020-08-04 18:55:45 +0530

    Add event and function modifier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 
@@ -20,13 +20,29 @@ function App() {
 
   const [account, setaccount] = useState(null);
 
-  const web3 = new Web3(Web3.currentProvider || "http://127.0.0.1:8545")
+  const [Loading, setLoading] = useState(false);
+
+  // const web3 = new Web3(Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+
+  var web3 = new Web3();
+  web3.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+
+
   web3.eth.getAccounts().then((result) => {
     setaccount(result[0]);
   })
 
 
   var coursetroContract = new web3.eth.Contract([
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "Coursetro",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
     {
       "constant": false,
       "inputs": [
@@ -46,6 +62,23 @@ function App() {
       "type": "function"
     },
     {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "name": "age",
+          "type": "uint256"
+        }
+      ],
+      "name": "Instructor",
+      "type": "event"
+    },
+    {
       "constant": true,
       "inputs": [],
       "name": "getInstructor",
@@ -63,28 +96,48 @@ function App() {
       "stateMutability": "view",
       "type": "function"
     }
-  ], "0x71a8761D8578bAF4af2871793F72DBa755D98767");
+  ], "0x57f85143ed7CA5026D22c7fBf5B45F0FF20DdbFe");
 
-  console.log(account);
+
 
   onchange = (event) => {
     setstate(({ ...state, [event.target.name]: event.target.value }));
   }
 
 
+  coursetroContract.getPastEvents('Instructor', {
+    fromBlock: 0,
+    toBlock: 'latest'
+  }, () => { })
+    .then(function (events) {
+      setLoading(false);
+      // console.log(events);
+      var getname = events[events.length - 1].returnValues['name'];
+      var getage = events[events.length - 1].returnValues['age']
+      setgetname(getname);
+      setgetage(getage);
+    });
+
+
+
 
   useEffect(() => {
-    coursetroContract.methods.getInstructor().call().then((result) => {
-      setgetname(result[0]);
-      setgetage(result[1]);
-      console.log(result);
-    })
+    // coursetroContract.methods.getInstructor().call().then((result) => {
+    //   setgetname(result[0]);
+    //   setgetage(result[1]);
+    // })
   })
 
 
 
   const setInstructor = () => {
-    coursetroContract.methods.setInstructor(state.name, state.age).send({ from: account });
+    coursetroContract.methods.setInstructor(state.name, state.age).send({ from: account }, (err, res) => {
+      setLoading(true);
+      if (err) {
+        setLoading(false);
+        console.log('oh no');
+      }
+    });
   }
 
 
@@ -93,21 +146,21 @@ function App() {
 
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <h2>Ethereum Blockchain</h2>
-        <form>
-          <div class="form-group">
-            <label for="email">Name:</label>
-            <input placeholder="Enter name" name="name" onChange={(event) => onchange(event)} value={state.name} />
-          </div>
-          <div class="form-group">
-            <label for="pwd">Age:</label>
-            <input placeholder="Enter age" name="age" onChange={(event) => onchange(event)} value={state.age} />
-          </div>
-          <button type="submit" class="btn btn-primary" onClick={() => setInstructor()}>Update Data</button>
+        {/* <form> */}
+        <div className="form-group">
+          <label htmlFor="email">Name:</label>
+          <input placeholder="Enter name" name="name" onChange={(event) => onchange(event)} value={state.name} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="pwd">Age:</label>
+          <input placeholder="Enter age" name="age" onChange={(event) => onchange(event)} value={state.age} />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={() => setInstructor()}>Update Data</button>
 
-        </form>
-        <div>{getname + ' ' + 'is' + ' ' + getage + ' ' + 'years old'}</div>
+        {/* </form> */}
+        <div>{Loading ? "Loading..." : getname + ' ' + 'is' + ' ' + getage + ' ' + 'years old'}</div>
       </div>
     </div>
   );
